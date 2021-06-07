18030021a3d12b3b0e1a13b58f42ad8b8b4bb6a7
renatalantos <renatalantos@gmail.com>
2021-06-07 15:46:30 +0000

    Enter key for submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,11 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     }
-
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition");
-    runGame("multiply");
-    runGame("subtract");
-    runGame("division");
+
 
 })
 /**
@@ -34,6 +36,9 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //Creates two random numbers between 1 and 25
 
@@ -46,7 +51,7 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-        
+
     } else if (gameType === "division") {
         displayDivisionQuestion(num1, num2);
     } else {
@@ -133,8 +138,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 
 }
@@ -147,7 +152,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
+
+
+    document.getElementById("operand1").textContent = operand1 % operand2 === 0 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
 
